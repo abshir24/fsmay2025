2f946bf569d62a792e37a01dc96f12e7aaee095b
abshir24 <abshirm@live.com>
2025-06-03 18:59:53 -0700

    added jsa9

--- Javascript/demo.js
+++ Javascript/demo.js
@@ -1,1 +1,3 @@
-console.log("This is the print statement from the external file")
+//Create a function that takes 2 parameters (start, end). Inside the function create a for loop
+//that starts at at start end ends at end. Inside the for loop print the numbers and whether or not they
+//are even or odd. Call the function and pass in the values. 
