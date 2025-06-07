6ba70a24e1291782d4d0e00b27ae28e2ff3e9267
abshir24 <abshirm@live.com>
2025-06-07 10:27:05 -0700

    added python a1

---
+++ Python/demo.py
@@ -0,0 +1,10 @@
+name = "Zues"
+
+name2 = "Hera"
+
+print( f"The full name above is {name} { name2 }")
+
+
+
+
+
---
+++ Python/pythonA1.py
@@ -0,0 +1,8 @@
+name = "Abshir Moham"
+
+# print( name.endswith("Mohamed") )
+
+
+username = "abshir24"
+
+print( username.isalnum())
