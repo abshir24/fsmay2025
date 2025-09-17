832adbb4e7c84efec5d305998d3b87900836e223
abshir24 <abshirm@live.com>
2025-09-16 18:24:07 -0700

    added redux example

--- React/demoapp/src/App.js
+++ React/demoapp/src/App.js
@@ -1,13 +1,49 @@
 import './App.css';
-import { useState } from 'react'
-import Counter from './components/ReactAssignment12/Counter';
+import { legacy_createStore as createStore } from 'redux';
+
+// action = {
+//   type: "PLUS",
+//   payload: 5
+// }
+
+const intialState = 0;
+
+const counterReducer = (state = intialState, action)=>{
+  switch(action.type){
+    case 'PLUS':{
+      return state + (action.payload)
+    }
+    case 'MINUS':{
+      return state - (action.payload)
+    }
+    default:{
+      return state
+    }
+  }
+}
+
+
+const store = createStore(counterReducer)
+
+const currentState = store.getState()
+
+console.log("State Before ", currentState)
+
+store.dispatch({ type:'PLUS', payload:2})
+
+const dispatch_1 = store.getState()
+
+console.log("State After dispatch 1 ", dispatch_1)
+
+store.dispatch({ type:'MINUS', payload:1})
+
+const dispatch_2 = store.getState()
+
+console.log("State After dispatch 2 ", dispatch_2)
 
 function App() {
-
   return (
-    <div>
-      <Counter />
-    </div>  
+    <h1>Hello World</h1>
   );
 }
 
--- React/demoapp/src/components/ReactAssignment12/Counter.js
+++ React/demoapp/src/components/ReactAssignment12/Counter.js
@@ -1,12 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function Counter() {
   const [counter, setCounter] = useState( 0 ) 
 
+  let updateCount = ()=>{
+    setCounter(counter + 1)
+  }
+
+  useEffect(()=>{
+    console.log(`Counter: ${counter}`)
+  },[counter])
+
   return (
     <div>
       <h1>Counter: { counter } </h1>
-      <button onClick={ () => setCounter(counter + 1) } >Increment +1</button>  
+      <button onClick={ updateCount } >Increment +1</button>  
       <button onClick={ () => setCounter(counter - 1) }>Decrement -1</button>  
     </div>  
   );
